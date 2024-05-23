32c2eb616bd56cf03ff3d5fe74e8a8bdbba2e07b
heppaat <pali.herrer@gmail.com>
2024-05-23 18:22:51 +0200

    using ref functions work as inteneded'

--- src/components/NameSearchResponse.vue
+++ src/components/NameSearchResponse.vue
@@ -1,11 +1,57 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div
+      v-if="result.isLucky && !isDuplicate"
+      class="flex flex-col items-center gap-4 mt-10 px-5"
+    >
+      <h1>
+        Congratulations, {{ result.name }} is a lucky name! You've won a prize.
+        Will you accept it?
+      </h1>
+      <button @click="accept" class="border-2 px-2 py-1">Accept</button>
+      <button @click="reject" class="border-2 px-2 py-1">Reject</button>
+    </div>
+
+    <div v-if="isDuplicate" class="flex flex-col items-center gap-4 mt-10 px-5">
+      <h1>
+        I'm sorry, {{ result.name }} has already received their prize today!
+      </h1>
+    </div>
+
+    <div
+      v-if="!result.isLucky"
+      class="flex flex-col items-center gap-4 mt-10 px-5"
+    >
+      <h1>
+        I'm sorry, {{ result.name }} is not in today's list of lucky names.
+      </h1>
+      <button @click="reject" class="border-2 px-2 py-1">Ok</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'NameSearchResponse',
-}
+  name: "NameSearchResponse",
+  props: {
+    result: {
+      type: Object,
+      required: true,
+    },
+    reject: {
+      type: Function,
+      required: true,
+    },
+    accept: {
+      type: Function,
+      required: true,
+    },
+    isDuplicate: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
--- src/components/NameSearcher.vue
+++ src/components/NameSearcher.vue
@@ -1,92 +1,122 @@
 <template>
   <div class="container">
-    <form @submit.prevent="handleSubmit">
+    <form
+      class="flex flex-col items-center gap-4 mt-10"
+      @submit.prevent="handleSubmit"
+    >
       <input
         class="border-2"
         type="text"
         v-model="inputName"
         placeholder="Enter name..."
       />
-      <button type="submit">Check</button>
+      <button class="border-2 px-2 py-1" type="submit">Check</button>
     </form>
+
+    <p v-if="error" class="flex mt-10">Error: {{ error }}</p>
+
+    <div v-if="result">
+      <name-search-response
+        :result="result"
+        :reject="handleReject"
+        :accept="handleAccept"
+        :isDuplicate="isDuplicate"
+      ></name-search-response>
+    </div>
+
+    <div
+      class="flex flex-col bg-[#f4f4f5] p-4 rounded-lg max-w-[350px] mt-10 mx-5"
+    >
+      <h1 class="text-[20px] mb-5">Today's winners:</h1>
+      <p v-for="(name, index) in winnersArray" :key="index">{{ name }}</p>
+    </div>
   </div>
-  <name-search-response></name-search-response>
 </template>
 
 <script>
 import NameSearchResponse from "@/components/NameSearchResponse.vue";
 import { getAll } from "../api/peopleApi";
+import { ref, watch } from "vue";
 
 export default {
   name: "NameSearcher",
   components: {
     NameSearchResponse,
   },
-  data() {
-    return {
-      inputName: "",
-      result: {
-        name: "",
-        isLucky: null,
-      },
-      error: "",
-      winnersArray: [],
-      isDuplicate: false,
-      accepted: false,
-    };
-  },
-  watch: {
-    accepted(newVal) {
+  setup() {
+    const inputName = ref("");
+    const result = ref(null);
+    const error = ref("");
+    const winnersArray = ref([]);
+    const isDuplicate = ref(false);
+    const accepted = ref(false);
+
+    watch(accepted, (newVal) => {
       if (newVal) {
-        this.inputName = "";
-        this.accepted = false;
+        inputName.value = "";
+        accepted.value = false;
       }
-    },
-  },
-  methods: {
-    async handleSubmit() {
-      await this.checkName();
-      console.log(this.result);
-    },
-    isLucky(input, array) {
+    });
+
+    const handleSubmit = async () => {
+      await checkName();
+      console.log(result.value);
+      console.log(isDuplicate.value);
+    };
+
+    const isLucky = (input, array) => {
       const lowerCaseInputName = input.toLowerCase().trim();
       return array.some(
         (person) => person.name.toLowerCase() === lowerCaseInputName
       );
-    },
-    async checkName() {
-      this.error = "";
+    };
+
+    const checkName = async () => {
+      error.value = "";
       try {
         const response = await getAll();
 
-        const luckyOrNot = this.isLucky(this.inputName, response);
+        const luckyOrNot = isLucky(inputName.value, response);
 
         const formattedInputName =
-          this.inputName.trim().charAt(0).toUpperCase() +
-          this.inputName.trim().slice(1).toLowerCase();
+          inputName.value.trim().charAt(0).toUpperCase() +
+          inputName.value.trim().slice(1).toLowerCase();
 
-        this.result = {
+        result.value = {
           name: formattedInputName,
           isLucky: luckyOrNot,
         };
 
-        this.isDuplicate = this.winnersArray.includes(formattedInputName);
+        isDuplicate.value = winnersArray.value.includes(formattedInputName);
       } catch (error) {
-        this.error = "An error occurred while fetching data";
-        console.log(this.error);
+        error.value = "An error occurred while fetching data";
+        console.log(error.value);
       }
-    },
+    };
 
-    handleReject() {
-      this.result = null;
-    },
-    handleAccept() {
-      this.accepted = true;
-      if (this.result && !this.isDuplicate) {
-        this.winnersArray.push(this.result.name);
+    const handleReject = () => {
+      result.value = null;
+    };
+
+    const handleAccept = () => {
+      accepted.value = true;
+      if (result.value && !isDuplicate.value) {
+        winnersArray.value.push(result.value.name);
       }
-      this.result = null;
-    },
+      result.value = null;
+    };
+
+    return {
+      inputName,
+      result,
+      error,
+      winnersArray,
+      isDuplicate,
+      accepted,
+      handleSubmit,
+      handleReject,
+      handleAccept,
+    };
   },
 };
 </script>
